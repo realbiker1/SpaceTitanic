7dcb03e5d7ecccf7d207cf7f33cc22f3ec3bfdd5
Anders-Marius <591327@stud.hvl.no>
2022-11-18 13:12:19 +0100

    ny forms

--- app/forms.py
+++ app/forms.py
@@ -1,29 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, FloatField, SelectField, SelectField, RadioField, BooleanField, SubmitField
+from wtforms import SubmitField, IntegerField, BooleanField, SelectField
 from wtforms.validators import DataRequired, NumberRange
-
 
 class DataForm(FlaskForm):
 
-    """
-    The form for entering values during patient encounter. Feel free to add additional 
-    fields for the remaining features in the data set (features missing in the form 
-    are set to default values in `predict.py`).
-    """
-
-    PassengerId = object('A unique Id for each passenger.')
-    HomePlanet = object('The planet the passenger departed from')
-
-    CryoSleep = object('Indicates if the passenger is "sleeping" in their cabins')
-    Cabin = object('The cabin number where the passenger is staying')
-
-    Destination = object('The planet the passenger will be debarking to.')
-    Age = FloatField('The age of the passenger', validators=[DataRequired()])
-    VIP = object('Wheter the passenger has paid for special VIP service during the voyage')
-    RoomService, Foodcourt, ShoppingMall, Spa, VRDeck = FloatField('Amount the passenger has billed these luxury amenities', validators=[DataRequired()])
-    Name = object('The first and last names of the passenger')
-    Transported = BooleanField(label='Transported')
-
-
+    age = IntegerField('Please enter age', validators=[NumberRange(min=0, max=99, message="Age must be between 0 and 99"), DataRequired(message="Age is required")])
+    cryosleep = BooleanField(label='Did you enter cryosleep during the trip?', validators=[DataRequired(message="Please check if you want to cryosleep")])
+    vrdeck = IntegerField('How much did you spend on the VRDeck', validators=[NumberRange(min=0, max=1000000, message="Value must be between 0 and 1000000")])
+    spa = IntegerField('How much did you spend on the ships spa', validators=[NumberRange(min=0, max=1000000, message="Value must be between 0 and 1000000")])
+    shoppingmall = IntegerField('How much did you spend on the shopping mall', validators=[NumberRange(min=0, max=1000000, message="Value must be between 0 and 1000000")])
+    roomservice = IntegerField('How much did you spend on roomservice', validators=[NumberRange(min=0, max=1000000, message="Value must be between 0 and 1000000")])
+    homeplanet = SelectField('What is your home planet?', choices=[('Earth'), ('Europa'), ('Mars')], validators=[DataRequired(message="Please select a homeplanet")])
+    destination = SelectField('What is your destination?', choices=[('TRAPPIST-1e'), ('55 Cancri e'), ('PSO J318.5-22')], validators=[DataRequired(message="Please select a destination")])
+    groupsize = IntegerField('How many people are you travelling with 0-10?', validators=[NumberRange(min=0, max=10, message="Value must be between 0 and 10"), DataRequired(message="Age is required")])
 
     submit = SubmitField('Submit')
